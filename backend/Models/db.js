b6a5779ffacf56efd82161df0f3e4b2b0ac68476
Shreya Pandey <dhriti240821@gmail.com>
2025-09-09 13:15:04 +0530

    changes made

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,10 +1,11 @@
+
 const mongoose = require('mongoose');
 
-const mongo_url = process.env.MONGO_CONN;
+const mongo_url = process.env.MONGO_URI; // Use the correct env variable
 
-mongoose.connect("mongodb://localhost:27017/expense")
+mongoose.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB Connected...');
     }).catch((err) => {
         console.log('MongoDB Connection Error: ', err);
-    })
+    });
